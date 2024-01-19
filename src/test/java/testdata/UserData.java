756c813528f4fad722ea274fbb65a2855358a760
Maria Borisova <mborisovaitd@yandex.ru>
2024-01-19 19:33:02 +0300

    Added test data class

---
+++ src/test/java/testdata/UserData.java
@@ -0,0 +1,9 @@
+package testdata;
+
+public class UserData {
+    public static final String CORRECT_EMAIL = "barabashka250@yandex.ru";
+    public static final String CORRECT_PASSWORD = "9876543210";
+    public static final String CORRECT_USER_NAME = "barabashka250";
+
+    public static final String SHORT_PASSWORD = "12345";
+}
