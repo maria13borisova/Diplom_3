b5504a8d93e474a36fc6a342c170537b37120418
Maria Borisova <mborisovaitd@yandex.ru>
2024-01-19 19:29:46 +0300

    Added constants

---
+++ src/main/java/constants/BurgerUrl.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class BurgerUrl {
+
+    public static final String HOME_PAGE = "https://stellarburgers.nomoreparties.site";
+    public static final String REGISTER_PAGE = HOME_PAGE + "/register";
+    public static final String LOGIN_PAGE = HOME_PAGE + "/login";
+    public static final String FORGOT_PASSWORD_PAGE = HOME_PAGE + "/forgot-password";
+    public static final String USER_ACCOUNT_PAGE = HOME_PAGE + "/account/profile";
+}
